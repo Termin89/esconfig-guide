1c313cee367eac855b8fe3a7f70af3796cecc295
Alexey <lifeprojob@gmail.com>
2023-04-20 17:57:14 +0300

    change tested file

--- tested.js
+++ tested.js
@@ -12,6 +12,9 @@ function g (      ) {
 
 
 
+const msg = 'his will include any commit attributed to your account but not signed with your GPG or S/MIME key. Note that this will include your existing unsigned commits.'
+
+
 
 
 
